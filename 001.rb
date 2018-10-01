23bc9ee124b63473d0a42208bf3aabda8b800da1
Elle H <itsellej@gmail.com>
2018-10-01 19:42:27 +0100

    Complete lecture 1 exercise 1

---
+++ 001.rb
@@ -0,0 +1,42 @@
+# 001 Exercise - real life objects
+#
+# Observe the real life objects around you. Create two lists for each object, each with 5 values:
+# - List 1: attributes, key / value pairs (describes object, detail, characteristic)
+# - List 2: methods / object's functionality (what can the object do)
+
+
+# WIRELESS HEADPHONES
+
+# Attributes:
+#
+# - Colour: black
+# - Size: small
+# - Earphones: 2
+# - Connection: bluetooth
+# - Buttons: 3
+# - Power: Rechargable internal battery
+#
+# Methods:
+#
+# - Play
+# - Stop
+# - Next Track
+# - Previous Track
+# - Connect to Bluetooth device
+
+
+# SHARPIE:
+
+# - Colour: gold
+# - Nib size: thin
+# - Length: 10cm
+# - Width: 1cm
+# - Ink level: full
+#
+# Methods:
+#
+# - Write
+# - Stop writing
+# - Throw across the room
+# - Scribble
+# - Draw
