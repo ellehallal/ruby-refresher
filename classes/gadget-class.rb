e222f4f9bda28eac6dcada6cafb008f41f9be5e9
Elle H <itsellej@gmail.com>
2018-10-15 12:42:12 +0100

    Add parameters to initialize method

--- classes/gadget-class.rb
+++ classes/gadget-class.rb
@@ -4,54 +4,25 @@ class Gadget
   attr_writer :password
   attr_reader :production_number
 
-  # #getter - read only
-  # def username #public instance method
-  #   @username #private instance method
-  # end
-  #
-  # #setter - write only
-  # def username=(username)
-  #   @username = username
-  # end
-  #
-  # #getter - read only
-  # def production_number
-  #   @production_number
-  # end
-  #
-  # #setter - write only
-  # def password=(password)
-  #   @password = password
-  # end
 
-
-  def initialize #private method
-    @username = "User #{rand(1..100)}" #read and write
-    @password = "topsecret" #write only
+  def initialize(username, password)
+    @username = username
+    @password = password
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..199)}" #read only
   end
 
-  def info  # a public method that can be called directly on the object
-    "Gadget #{@production_number} has the username #{@username}"
-  end
 
   def to_s  # a public method that can be called directly on the object
     "Overriding the to_s method inherited from the object class"
+    "Gadget #{@production_number} has the username #{@username}"
   end
 end
 
-phone = Gadget.new
-watch = Gadget.new
+g1 = Gadget.new("myusername", "mypassword")
+g2 = Gadget.new("msprogrammer", "mypasswordisexcellent")
 
-puts phone.info
-puts watch.to_s
+p g1.username
+p g2.username
 
-
-#read only instance methods / getter methods
-p phone.username
-p phone.production_number
-
-#write only instance methods / setter methods
-
-p phone.username=("mynewusername")
-p phone.password=("mynewpassword")
+g1.username = "newusername"
+p g1.username
