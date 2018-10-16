efda726bc6e389b5943c44318a86f78dcb14612c
Elle H <itsellej@gmail.com>
2018-10-16 08:45:43 +0100

    Create Length Conversions module

---
+++ modules/length-conversions.rb
@@ -0,0 +1,23 @@
+module LengthConversions
+
+
+  WEBSITE = "http://website.com" #constant
+
+  def self.miles_to_feet(miles)
+    miles * 5280
+  end
+
+  def self.miles_to_inches(miles)
+    feet = miles_to_feet(miles)
+    feet * 12
+  end
+
+  def self.miles_to_cm(miles)
+    inches = miles_to_inches(miles)
+    inches / 2.5
+  end
+end
+
+puts LengthConversions.miles_to_feet(100)
+puts LengthConversions.miles_to_inches(200)
+puts LengthConversions.miles_to_cm(300)
